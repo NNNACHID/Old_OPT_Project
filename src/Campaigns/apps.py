2063c2b8086f6b8fa8e163674313b46ca6a4a8b5
NNNACHID <n.n.nachid@protonmail.com>
2024-05-15 21:25:58 +0200

    new arch

--- src/Campaigns/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CampaignsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Campaigns'
-
-    def ready(self):
-        import Campaigns.signals
--- src/Users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Users.models import *
-
-
-admin.site.register(CustomUser)
-admin.site.register(CustomUserProfile)
