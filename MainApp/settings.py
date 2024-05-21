de76f683edcb34d834137dc15c0bb5ea44b71cf8
NNNACHID <n.n.nachid@protonmail.com>
2024-05-21 12:05:56 +0200

    postgres sql, gunicorn

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 AUTH_USER_MODEL = "Users.CustomUser"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
@@ -92,8 +92,12 @@ WSGI_APPLICATION = "MainApp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "opt",
+        "USER": "optuser",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
@@ -134,6 +138,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
